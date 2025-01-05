2227eeb085a8da3bab97d64242e40931ac5dd6e7
Full Name <theofficialwebsiteguys@gmail.com>
2025-01-05 12:10:50 -0500

    git ignore

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: true,
-    api_url: 'https://ecommerce-api-db-a62e801c0f22.herokuapp.com/api',
-    api_key: '5ec3d75e059e3e70c67ae066f08433ddbb7a592e2ec295ceeb7020e51b70584b',
-    stripePublicKey: 'pk_live_51QUtysIokBL8AG53iemL67BNVGPhlPuX3OP3N0KiMM9IlDGZcJhF3KZezjdUBjqJqt7NKFfk1DkJLQCGDRxN5MGH00wpDEUC16'
-  };
-  
--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: false,
-    api_url: 'http://localhost:3333/api',
-    api_key: '5ec3d75e059e3e70c67ae066f08433ddbb7a592e2ec295ceeb7020e51b70584b',
-    stripePublicKey: 'pk_test_51QUtysIokBL8AG53jGjmeRtOdC3fQzboBQf8YMC4IdvoAxld2Ey9uVqgjce3cRvlzZG7ZxJzkKQbTJiFOfbmAHlw00fJEI31b1'
-  };
-  
